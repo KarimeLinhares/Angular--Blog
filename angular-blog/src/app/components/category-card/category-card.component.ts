b5cb093e7f415fd3a7ca963d52e0b5e3722bac45
KarimeLinhares <karime_muniz@hotmail.com>
2024-01-05 09:38:12 -0300

    add props to article section

--- angular-blog/src/app/components/category-card/category-card.component.ts
+++ angular-blog/src/app/components/category-card/category-card.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-category-card',
   templateUrl: './category-card.component.html',
-  styleUrls: ['./category-card.component.css']
+  styleUrls: ['./category-card.component.css'],
 })
 export class CategoryCardComponent {
-
+  @Input()
+  Title: string = '';
+  @Input()
+  More: string = '';
+  @Input()
+  PhotoCover1: string = '';
+  @Input()
+  PhotoCover2: string = '';
+  @Input()
+  PhotoCover3: string = '';
+  @Input()
+  PhotoCover4: string = '';
+  @Input()
+  CardTitle1: string = '';
+  @Input()
+  CardTitle2: string = '';
+  @Input()
+  CardTitle3: string = '';
+  @Input()
+  CardTitle4: string = '';
 }
